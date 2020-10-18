6cfb6537e35857295587df8db2e39662910d5677
RadmirSad <62589656+RadmirSad@users.noreply.github.com>
2020-10-18 21:08:44 +0300

    Add files via upload

--- Project3_1/BinVector.cpp
+++ Project3_1/BinVector.cpp
@@ -111,7 +111,7 @@ BinVector BinVector::AND(BinVector b)
 
 BinVector BinVector::XOR(BinVector b)
 {
-		int max = 0, secind = 0, ind;
+	int max = 0, secind = 0, ind;
 	char* maxvect = nullptr, *secvect = nullptr;
 	std::string str;
 	if (index > b.getSize())
@@ -132,7 +132,7 @@ BinVector BinVector::XOR(BinVector b)
 		if (maxvect[max - ind] == secvect[secind - ind]) str += '0';
 		else str += '1';
 	for (ind = max - secind - 1; ind >= 0; ind--)		
-		if (maxvect[max - ind] == '1') str += '1';
+		if (maxvect[ind] == '1') str += '1';
 		else str += '0';
 	for (int i = 0, j = str.length() - 1; i < j; i++, j--)
 		std::swap(str[i], str[j]);
@@ -144,8 +144,8 @@ BinVector BinVector::DOP()
 {
 	std::string str;
 	for (int i = 0; i < index; i++)
-		if (ar[i] == '0') str += '0';
-		else str += '1';
+		if (ar[i] == '0') str += '1';
+		else str += '0';
 	BinVector res(str);
 	return res;
 }
@@ -163,4 +163,135 @@ BinVector BinVector::WN()
 		str += ar[fir];
 	BinVector res(str);
 	return res;
+}
+
+void GetOR(BinVector my_vect)
+{
+	BinVector sec = GetSecVec();
+	sec = my_vect.OR(sec);
+	char* buf = sec.getVec();
+	int ind = sec.getSize();
+	std::cout << "Итоговый вектор : { ";
+	for (int i = 0; i < ind; i++)
+	{
+		std::cout << buf[i];
+		if (i + 1 != ind) std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
+}
+
+void GetAND(BinVector my_vect)
+{
+	BinVector sec = GetSecVec();
+	sec = my_vect.AND(sec);
+	char* buf = sec.getVec();
+	int ind = sec.getSize();
+	std::cout << "Итоговый вектор : { ";
+	for (int i = 0; i < ind; i++)
+	{
+		std::cout << buf[i];
+		if (i + 1 != ind) std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
+}
+
+void GetXOR(BinVector my_vect)
+{
+	BinVector sec = GetSecVec();
+	sec = my_vect.XOR(sec);
+	char* buf = sec.getVec();
+	int ind = sec.getSize();
+	std::cout << "Итоговый вектор : { ";
+	for (int i = 0; i < ind; i++)
+	{
+		std::cout << buf[i];
+		if (i + 1 != ind) std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
+}
+
+void GetDOP(BinVector my_vect)
+{
+	BinVector sec = my_vect.DOP();
+	char* buf = sec.getVec();
+	int ind = sec.getSize();
+	std::cout << "Итоговый вектор : { ";
+	for (int i = 0; i < ind; i++)
+	{
+		std::cout << buf[i];
+		if (i + 1 != ind) std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
+}
+
+void GetWN(BinVector my_vect)
+{
+	BinVector sec = my_vect.WN();
+	char* buf = sec.getVec();
+	int ind = sec.getSize();
+	std::cout << "Итоговый вектор : { ";
+	for (int i = 0; i < ind; i++)
+	{
+		std::cout << buf[i];
+		if (i + 1 != ind) std::cout << ", ";
+	}
+	std::cout << " }" << std::endl;
+}
+
+void SetVect(BinVector& vect)
+{
+	int dop_ent = 0, dop_sw = 0, & link = dop_sw;
+	std::cout << "Выберите способ инициализации второго вектора:" << std::endl
+		<< "1. Двоичное число." << std::endl
+		<< "2. Десятичное число" << std::endl;
+	while (dop_ent == 0)
+	{
+		Get_num(link, 0);
+		switch (dop_sw)
+		{
+		case 1:
+			std::cout << "Введите число:" << std::endl;
+			vect.setVec(10);
+			dop_ent = 1;
+			break;
+		case 2:
+			std::cout << "Введите число:" << std::endl;
+			vect.setVec(2);
+			dop_ent = 1;
+			break;
+		default:
+			std::cout << "Способа с таким номером не существует. Попробуйте снова:" << std::endl;
+			break;
+		}
+	}
+}
+
+BinVector GetSecVec()
+{
+	BinVector sec;
+	int dop_ent = 0, dop_sw = 0, & link = dop_sw;
+	std::cout << "Выберите способ инициализации второго вектора:" << std::endl
+		<< "1. Двоичное число." << std::endl
+		<< "2. Десятичное число" << std::endl;
+	while (dop_ent == 0)
+	{
+		Get_num(link, 0);
+		switch (dop_sw)
+		{
+		case 1:
+			std::cout << "Введите число:" << std::endl;
+			sec.setVec(10);
+			dop_ent = 1;
+			break;
+		case 2:
+			std::cout << "Введите число:" << std::endl;
+			sec.setVec(2);
+			dop_ent = 1;
+			break;
+		default:
+			std::cout << "Способа с таким номером не существует. Попробуйте снова:" << std::endl;
+			break;
+		}
+	}
+	return sec;
 }
