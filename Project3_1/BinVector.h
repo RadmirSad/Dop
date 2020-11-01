cea7e4dc716985fe075fddeee9c55e71795a6ebd
RadmirSad <62589656+RadmirSad@users.noreply.github.com>
2020-11-01 13:47:56 +0300

    Add files via upload

--- Project3_1/BinVector.h
+++ Project3_1/BinVector.h
@@ -12,47 +12,53 @@ private:
 	int index;
 	char ar[SZ];
 public:
-	BinVector() : index(1) { ar[0] = '0'; ar[1] = '\0'; }
+	BinVector() : index(1) { ar[0] = '0'; }
 	BinVector(int a);
-	BinVector(std::string str);
+	BinVector(const std::string & str);
 	void setVec(int div);
-	char* getVec() { return ar; }
-	BinVector OR(BinVector b);
-	BinVector AND(BinVector b);
-	BinVector XOR(BinVector b);
-	BinVector DOP();
-	BinVector WN();
+	const char * getVec() const { return ar; }
+	void OR (const BinVector& b, BinVector& res);
+	void AND(const BinVector& b, BinVector& res);
+	void XOR(const BinVector& b, BinVector& res);
+	void DOP(BinVector& res);
+	void WN(BinVector& res);
+	void GetMax(char const*& secvect, char const *& maxvect, const BinVector& b, int& max, int& secind);
+	void operator () (const char* str, int len);
 	int getSize() const { return index; }
 	int getMaxSize() const { return SZ; }
 };
 
-void GetOR(BinVector my_vect);
-void GetAND(BinVector my_vect);
-void GetXOR(BinVector my_vect);
-void GetDOP(BinVector my_vect);
-void GetWN(BinVector my_vect);
-void SetVect(BinVector& link);
-
-BinVector GetSecVec();
+void GetOR(BinVector& my_vect);
+void GetAND(BinVector& my_vect);
+void GetXOR(BinVector& my_vect);
+void GetDOP(BinVector& my_vect);
+void GetWN(BinVector& my_vect);
+void GetSecVec(BinVector& vect);
 
 template <class number>
 int Get_info(number& a, int flag_for_vect)
 {
-	int flag = 0;
+	int flag = 0, dop_count = 0, buf = pow(2, sizeof(a)*8 - 1);
 	std::string dop;
 	getline(std::cin, dop);
-	if (dop.length() == 0) return 0;
+	if (dop.length() == 0) return 1;
+	while (buf != 0)
+	{
+		buf /= 10;
+		dop_count++;
+	}
+	if (dop.length() > dop_count) return 2;
 	for (int i = 0; i < dop.length(); i++)
 		if (flag_for_vect > 0) // 1 - for vect; 0 - for int
 		{
 			if ((dop[i] < 48) || (dop[i] > 49)) flag = 1;
 		}
 		else if ((dop[i] < 48) || (dop[i] > 57)) flag = 1;
-	if (flag) return 0;
+	if (flag) return 3;
 	std::stringstream ss;
 	ss << dop;
 	ss >> a;
-	return 1;
+	return 0;
 }
 
 template <class number>
@@ -60,8 +66,21 @@ void Get_num(number& num, int flag_for_vect)
 {
 	int flag = 0;
 	while (!flag)
-		if (Get_info(num, flag_for_vect) > 0) flag = 1;
-		else std::cout << "РќРµРІРµСЂРЅС‹Р№ С„РѕСЂРјР°С‚ РІРІРѕРґР°. Р’РІРµРґРёС‚Рµ СЃРЅРѕРІР°:" << std::endl;
+		switch (Get_info(num, flag_for_vect))
+		{
+		case 0:
+			flag = 1;
+			break;
+		case 1:
+			std::cout << "Неверный формат ввода. Введите снова:" << std::endl;
+			break;
+		case 2:
+			std::cout << "Возникло переполнение. Введите снова:" << std::endl;
+			break;
+		case 3:
+			std::cout << "Присутствие недопустимых символов. Введите снова:" << std::endl;
+			break;
+		}
 }
 
 #endif
